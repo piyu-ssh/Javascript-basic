ab46af680b6b90fb30ac2bbaf366d9a51e4db91e
piyu-ssh <piyushgupta550099@gmail.com>
2025-01-24 23:17:43 +0530

    update 02_data*.js

--- 23-Jan/02_datatype_conversion.js
+++ 23-Jan/02_datatype_conversion.js
@@ -4,9 +4,18 @@ console.log(typeof numberVar);
 
 // string
 let strNum = "123"
+let strName = "String"
 let conStrNum = Number(strNum)
-console.log(typeof conStrNum);
+let conStrName = Number(strName)
 console.log(typeof strNum);
+console.log(strNum);
+console.log(typeof conStrNum);
+console.log(conStrNum);
+console.log(typeof strName);
+console.log(strName);
+console.log(typeof conStrName);
+console.log(conStrName);
+
 
 // null
 let nullNum = null
@@ -32,4 +41,11 @@ console.log(boolVal);
 console.log(typeof numBoolVal);
 console.log(numBoolVal);
 
+// boolean
+let isLoggedIn = 1
+// let isLoggedIn = "" // false
+// let isLoggedIn = "Piyush" // true
+let booleanUser = Boolean(isLoggedIn)
+console.log(booleanUser);
+console.log(typeof booleanUser);
 
