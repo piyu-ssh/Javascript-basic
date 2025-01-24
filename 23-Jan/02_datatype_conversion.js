e9d3eb1d6bf903eed1dcf9c4a238e41bdeb37f6c
piyu-ssh <piyushgupta550099@gmail.com>
2025-01-24 20:45:22 +0530

    update 02_datatype_conversion.js

--- 23-Jan/02_datatype_conversion.js
+++ 23-Jan/02_datatype_conversion.js
@@ -1,26 +1,25 @@
-let num1 = 21
-let var1 = "33"
+// number
+let numberVar = 123
+console.log(typeof numberVar);
 
-console.log(typeof num1);
-console.log(typeof var1);
+// string
+let strNum = "123"
+let conStrNum = Number(strNum)
+console.log(typeof conStrNum);
+console.log(typeof strNum);
 
-// itna to basic ha, main cheez yaha se suru ho rhi ha ...
-// let say vaiable ko ham convert kar dete ha Number me,
+// null
+let nullNum = null
+let numNullVal = Number(nullNum)
+console.log(typeof nullNum);
+console.log(nullNum);
+console.log(typeof numNullVal);
+console.log(numNullVal);
 
-let conVar = Number(var1);
-// aab iska type or value check karo, 
-
-console.log(conVar);
-console.log(typeof conVar);
-// ye number to aa gaya, but kya jarruri ha ki variable me sirf number hi ho ????
-
-let var2 = "123abc"
-let conVar2 = Number(var2)
-// jaise aisa koi value ho, to iska datatype or value kya hoga ? (Let's see)
-console.log(var2);
-console.log(typeof conVar2);
-// Ye bhi number print de diya, but ye var2 ka datatype kya ha ???
-console.log(typeof var2); // string datatype ha ... 
-// but conVar2 me data ha kya ???
-console.log(conVar2); // NaN datatype ha ye!
-
+// undefined
+let undeVal = undefined
+let numUndeVal = Number(undeVal)
+console.log(typeof undeVal);
+console.log(undeVal);
+console.log(typeof numUndeVal);
+console.log(numUndeVal);
