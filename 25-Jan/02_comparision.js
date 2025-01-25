329df3a5fe511c13f906d4be0bf6223d4271daca
piyu-ssh <piyushgupta550099@gmail.com>
2025-01-25 12:40:02 +0530

    add new folder '25-Jan'

---
+++ 25-Jan/02_comparision.js
@@ -0,0 +1,16 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2>=1);
+console.log(2==="2")
+console.log(2===2) // triple equal to checks the value and the type of the value
+console.log(2=="2") // double equal to checks only the value
+console.log(2!==2) // triple not equal to checks the value and the type of the value
+console.log(2!="2") // double not equal to checks only the value
+
+
+console.log("02" == 2) 
+// Output: true
+
+console.log("22" === 22);
+// Output: false
+
