772910ee5db6d0e509fe976a3807194fe0550246
piyu-ssh <piyushgupta550099@gmail.com>
2025-01-27 00:46:29 +0530

    add Web-Dev folder with crucial files

---
+++ Web-Dev/server.js
@@ -0,0 +1,5 @@
+const btn1 = document.getElementById('btn1');
+
+btn1.onclick = function () {
+    alert('I made this while practicing JS!\nYou can see the code in the "25-Jan/ 05_string.js" file');
+}
