ed904c6c4c68cbc85a6065f661206846fefef963
piyu-ssh <piyushgupta550099@gmail.com>
2025-01-27 11:04:50 +0530

    add math in 01_num*.js

--- 26-Jan/01_num_and_maths.js
+++ 26-Jan/01_num_and_maths.js
@@ -22,3 +22,31 @@ console.log(anotherNum.toPrecision(4));
 
 const myCurrentBankBalance = 9999909990;
 console.log(myCurrentBankBalance.toLocaleString('en-IN'));
+
+// Maths
+// Maths ka object ha, isme bohot sare methods ha,
+console.log(Math.PI);
+console.log(Math.round(7.544568676854543));
+console.log(Math.abs(-5464));
+
+// Math ki or properties hain, 
+// min max rand floor ceil
+
+const min = 10
+const max = 20
+
+console.log(Math.random()); // ye random number generate karta ha btw 0 to 1 except 1,
+const numGen = Math.floor(Math.random() * 10); // ye random number generate karta ha btw 0 to 9,
+
+console.log(`This will generate a random number btw 0-9 -> ${numGen}`);
+
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min)); // ye random number generate karta ha btw 10 to 20,
+// thoda breakdown karte ha isko, 
+
+// 1. Math.random() -> 0 to 1 except 1,
+// 2. Math.random() * (max - min + 1) -> 0 to 10,
+// 3. Math.random() * (max - min + 1) + min -> 10 to 20,
+// 4. Math.floor(Math.random() * (max - min + 1) + min) -> 10 to 20,
+
+
