1ced7a92f090571ff0f8c2d853c9046faed400b5
piyu-ssh <piyushgupta550099@gmail.com>
2025-01-27 17:46:58 +0530

    add 02_date.js

---
+++ 26-Jan/02_date.js
@@ -0,0 +1,21 @@
+const newDate = new Date();
+
+console.log(newDate);
+console.log(newDate.toString());
+console.log(newDate.toDateString());
+console.log(newDate.toTimeString());
+console.log(newDate.toISOString());
+console.log(newDate.toLocaleDateString());
+
+// const myCreatedDate = new Date (2020, 0, 1);
+// console.log(myCreatedDate.toDateString());
+// ye custom date print kar dega!
+let myCreatedDate = new Date ("12-01-2025");
+console.log(myCreatedDate.toLocaleString());
+
+// Date
+
+let currTime = Date.now();
+console.log(currTime);
+
+// ! ye or sikhenge isko project me implement karna parega!!!
